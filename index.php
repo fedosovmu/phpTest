6475870f0a12693199a89d9a66764646d3cbff08
fedosovmu <fedosovmu@gmail.com>
2017-09-03 22:41:36 +0500

    added html

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php
-echo 'hello';
-echo '<br>';
-echo 7 + 99;
-?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Тестируем PHP</title>
+</head>
+<body>
+<?php echo '<p>Привет, мир!</p>'; ?>
+</body>
+</html>
